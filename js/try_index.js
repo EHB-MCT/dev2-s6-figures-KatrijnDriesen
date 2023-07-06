24a5367aafea799a2378289533a954fd2976590c
Katrijn Driesen <katrijn.driesen@student.ehb.be>
2023-07-06 15:25:56 +0200

    changeSelect finished

--- js/try_index.js
+++ js/try_index.js
@@ -9,27 +9,31 @@ const form = {
     changeSelect(e) {
         console.log("log changeSelect functie");
         console.log(this.value);
-        const inputFields = document.getElementById("otherInputs");
-        inputFields.innerHTML = "";
-        if (this.value == "Circle") {
-            inputFields.innerHTML = this.inputElementsWithLabel("radius (px)","radius");
-        } else if (this.value == "Square") {
-            inputFields.innerHTML = this.inputElementsWithLabel("size (px)","size");
-        } else if (this.value == "Rectangle") {
-            inputFields.innerHTML = this.inputElementsWithLabel("width (px)","width");
-            inputFields.innerHTML += this.inputElementsWithLabel("height (px)","height");
+        
+        const inputFields = document.getElementById('otherInputs');
+        let htmlString = "";
+        
+        if (this.value == "circle") {
+            htmlString += form.inputElementsWithLabel("radius (px)","radius");
+        } else if (this.value == "square") {
+            htmlString += form.inputElementsWithLabel("size (px)","size");
+        } else if (this.value == "rectangle") {
+            htmlString += form.inputElementsWithLabel("width (px)","width");
+            htmlString += form.inputElementsWithLabel("height (px)","height");
         }
+        inputFields.innerHTML = htmlString;
     },
     init() {
         //console.log('Form init!');
-        document.getElementById("select").addEventListener('change', this.changeSelect);
+        document.getElementById('select').addEventListener('change', this.changeSelect);
         document.getElementById("form").addEventListener('submit',(event) => this.submitForm(event));
     },
     inputElementsWithLabel(label, id) {
-       /*  <label>
-        YOUR LABEL NAME
-        <input id="YOUR INPUT ID" type="number">
-        </label> */
+        return `
+        <label>
+        ${label}
+        <input id="${id}" type="number">
+        </label>`;
     },
     submitForm(event) {
         console.log("log submitForm functie");
